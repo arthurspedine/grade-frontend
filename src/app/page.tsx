f5b2c1b79167ae2bb0a638963b0283795ee1b838
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2024-12-06 12:26:53 -0300

    chore: remove unused pages & components + adjust main page - Deleted unused routes and pages: csr, ssr, external, profile. - Removed outdated NavBar component. - Adjusted main page to include a welcome message. - Removed unused API endpoint: message route.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
-
-export default function Index() {
-  return <div>oi</div>
+export default function MainPage() {
+  return (
+    <div className='flex flex-col flex-grow'>
+      <section className='max-w-[1440px] mx-auto w-full px-8'>
+        <h1 className='font-bold text-2xl'>Bem-vindo!</h1>
+      </section>
+    </div>
+  )
 }
