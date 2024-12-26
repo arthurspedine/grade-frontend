a80e73daf0b330e82b6019b7770ab4d68ab51029
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2024-12-26 15:10:55 -0300

    feat: add user dropdown menu to header
    - Implement dropdown menu for logged-in users
    - Display user name, email, and logout option
    - Add click outside listener to close dropdown
    - Implement smooth fade-in animation for dropdown
    - Update Tailwind config with new animation style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,9 +5,36 @@ import grade_logo from '/public/grade_logo.svg'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { Skeleton } from '../ui/skeleton'
+import { useEffect, useRef, useState } from 'react'
 
 export function Header() {
   const { user, isLoading } = useUser()
+
+  const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef<HTMLDivElement | null>(null)
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
 
   return (
     <header className='flex items-center justify-between pr-14 max-w-[1440px] mx-auto w-full'>
@@ -35,13 +62,42 @@ export function Header() {
             <li>Avaliações</li>
           </ul>
 
-          <Image
-            src={user.picture ? user.picture : ' '}
-            alt='Foto do usuario'
-            width={48}
-            height={48}
-            className='rounded-full'
-          />
+          <div className='relative inline-block text-left'>
+            <button
+              ref={buttonRef}
+              onClick={toggleDropdown}
+              className='rounded-full hover:bg-transparent focus:border-none focus:outline-none font-medium text-sm text-center inline-flex items-center'
+              type='button'
+            >
+              <Image
+                src={user.picture ? user.picture : ' '}
+                alt='Foto do usuario'
+                width={48}
+                height={48}
+                className='rounded-full'
+              />
+            </button>
+
+            {isOpen && (
+              <div
+                ref={dropdownRef}
+                className='z-10 bg-muted divide-y divide-muted-foreground-50 rounded-lg shadow absolute right-0 animate-div-fade-in-down'
+              >
+                <div className='px-4 py-3 text-sm text-primary animate-fade-in-down'>
+                  <p>{user.name}</p>
+                  <p className='font-medium truncate'>{user.email}</p>
+                </div>
+                <div className='mt-2 animate-fade-in-down'>
+                  <a
+                    href='/api/auth/logout'
+                    className='block px-4 py-2 text-sm text-primary hover:border-b-2 hover:border-x-2 rounded-b-lg transition-all duration-300'
+                  >
+                    Sair
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       )}
       {!isLoading && !user && (
